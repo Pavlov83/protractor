7af78efbc9a2e6b54dc20f459562638b65fa6684
Pavlov83 <resolversoft@gmail.com>
2021-04-26 21:21:29 +0300

    more methods added

--- testcases/firstTestScript.js
+++ testcases/firstTestScript.js
@@ -15,34 +15,56 @@ describe("suite1", function(){
 
     })
 
-    it("fill company name",function(){
+    it("fills company name",function(){
         element(by.css("input[formcontrolname=company]")).click().sendKeys('ResolverSoft');
         browser.sleep(2000);          
            
     })  
 
-    it("fill first name",function(){
+    it("fills first name",function(){
         element(by.css("input[formcontrolname=firstName]")).click().sendKeys('Pavel');
         browser.sleep(2000);          
            
     })  
 
-    it("fill last name",function(){
+    it("fills last name",function(){
         element(by.css("input[formcontrolname=lastName]")).click().sendKeys('Pavlov');
         browser.sleep(2000);          
            
-    })  
+    })
 
+    it("fills address",function(){
+        element(by.css("textarea[formcontrolname=address]")).click().sendKeys('Bulgaria,test,adress,1');
+        browser.sleep(2000);          
+           
+    })
 
+    it("clicks on +Add",function(){
+        element(by.xpath("//*[contains(text(),' + Add C/O, Apt, Suite, Unit ')]")).click();
+        browser.sleep(2000); 
+    })
 
+    it("fills city",function(){
+        element(by.css("input[formcontrolname=city]")).click().sendKeys('Bulgaria,test,adress,1');
+        browser.sleep(2000);          
+           
+    })
+    
+    it("Clicks on State",function(){
+        element(by.xpath('//*[@class="mat-select-placeholder mat-select-min-line ng-tns-c95-7 ng-star-inserted"]')).click();
+        browser.sleep(2000); 
+                 
+           
+    })
 
-
-
+    it("fills city",function(){
+        element(by.css("input[formcontrolname=city]")).click().sendKeys('Sofia');
+        browser.sleep(2000);          
+           
+    })
 
     it("click submit",function(){
         element(by.buttonText("Submit")).click();
         browser.sleep(2000);  
     })
-
-
 })
